c30700482a5bd8a91412731dc93758f7b5948b0c
taroj khan <tarujkhan@yahoo.com>
2020-03-20 21:59:47 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -24,4 +24,25 @@ class Author
 end
     
     
-    
+    class Song 
+  attr_accessor :name, :artist
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+
+def artist_name
+  if @artist == nil 
+    return nil
+  else 
+    @artist.name
+  end 
+  end 
+end 
