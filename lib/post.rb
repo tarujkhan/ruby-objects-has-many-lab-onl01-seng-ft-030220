9a5848411d3817dadad2f001099936711bbf520f
taroj khan <tarujkhan@yahoo.com>
2020-03-20 21:58:14 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,16 +1,9 @@
 class Post 
   attr_accessor :title, :author 
   
-  @@all = []
-  
   def initialize(title)
     @title = title
-    @@all << self
   end
-  
-  def self.all
-    @all 
-  end 
   
   def author_name
     if @author == nil 
@@ -22,25 +15,3 @@ class Post
 end 
 
 
-class Song 
-  attr_accessor :name, :artist
-  
-  @@all = []
-  
-  def initialize(name)
-    @name = name 
-    @@all << self 
-  end 
-  
-  def self.all 
-    @@all 
-  end 
-
-def artist_name
-  if @artist == nil 
-    return nil
-  else 
-    @artist.name
-  end 
-  end 
-end 
