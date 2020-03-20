30784db69a0f1c0b27314ecd20453dccaaebc153
taroj khan <tarujkhan@yahoo.com>
2020-03-20 22:24:44 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,38 @@
+class Author 
+  attr_accessor :name, :posts 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @posts = []
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end 
+  
+ def posts
+   Post.all.select {|post| post.author == self}
+  end 
+ 
+  def add_post(post)
+      @posts << post
+      post.author = self
+   
+    end
+      
+    
+    def add_post_by_title(title)
+      post = Post.new(title)
+     add_post(post) 
+    end 
+    
+  
+      def self.post_count
+       Post.all.count 
+      end 
+      
+     
+end
