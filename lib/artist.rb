2bcc06289be3397e4804e7122b72590581aeca90
taroj khan <tarujkhan@yahoo.com>
2020-03-20 21:38:08 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,11 +2,17 @@ class Artist
   attr_accessor :name
   
   @@song_count = 0 
+  @@all = []
   
   def initialize(name)
     @name = name 
     @songs = []
   end 
+  
+  def self.all
+    @@all 
+  end 
+  
   
   def songs 
   end 
